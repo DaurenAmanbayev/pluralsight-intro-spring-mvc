e9f46694ffbdfcd7e7eecbc33deb8f2b9ba28ca8
Daniel Arlitt <dgarlitt@yahoo.com>
2015-01-16 14:44:14 -0800

    Section completed - Controllers

---
+++ src/main/java/com/pluralsight/model/Exercise.java
@@ -0,0 +1,18 @@
+package com.pluralsight.model;
+
+/**
+ * Created by daniel.arlitt on 1/16/15.
+ */
+public class Exercise {
+
+  private int minutes;
+
+  public int getMinutes() {
+    return minutes;
+  }
+
+  public void setMinutes(int minutes) {
+    this.minutes = minutes;
+  }
+
+}
